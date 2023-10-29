ab871733711a9087216eae366b286e9816d09a8d
Rajesh Sahani <143243038+RajeshSahani17@users.noreply.github.com>
2023-10-29 14:15:25 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+//let score = null
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/*score = undefined
+console.log(typeof score);
+console.log(typeof(score));
+ valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+// 33 => 33
+// "33abs" => NaN( Not A Number)
+// true => 1 , false => 0
+
+let isLoggedIn = 1
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "rajesh" => true
