6b221bd1c56fcc0de84ab724d39918f08b4cbd8c
Rajesh Sahani <143243038+RajeshSahani17@users.noreply.github.com>
2023-10-29 14:43:16 +0000

    Operations Added

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,8 +21,8 @@ console.log(valueInNumber);*/
 let isLoggedIn = 1
 
 let bolleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof bolleanIsLoggedIn);
-console.log(isLoggedIn);
+//console.log(typeof bolleanIsLoggedIn);
+//console.log(isLoggedIn);
 
 // 1 => true ; 0 => false
 // "" => false
@@ -31,5 +31,49 @@ console.log(isLoggedIn);
 isLoggedIn = 33
 
 let stringIsLoggedIn = String(isLoggedIn)
-console.log(typeof stringIsLoggedIn);
-console.log(isLoggedIn);
+//console.log(typeof stringIsLoggedIn);
+//console.log(isLoggedIn);
+
+// ########### Operations ###############
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)
+//console.log(2/3)
+//console.log(2%3)
+
+let str1 = "helllo"
+let str2 =" rajesh"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2); // o/p => 12
+//console.log(1 + "2");// o/p => 12
+//console.log("1" + 2 + 2);// o/p => 122
+//console.log(1 + 2 + "2");// o/p => 32 
+
+//console.log( (3 + 4) * 5 %2);
+
+//console.log(true); // o/p => true
+//console.log(+true);// o/p => 1
+//console.log(+""); // o/p => 0
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+//gameCounter++;
+//console.log(gameCounter);// o/p => 101
+++gameCounter;
+console.log(gameCounter); // o/p => 101
+
+
+ 
+
