fbcb6716ec256796d0a8074db8c3bd40c2886cb1
Rajesh Sahani <143243038+RajeshSahani17@users.noreply.github.com>
2023-10-29 13:48:11 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using node.js , not browser
+
+console.log(3+3); 
+console.log("rajesh"); // code readability should be high
+
+let name = "Rajesh"
+let age = 23
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value(object)
+// undefined => 
+// symbol =>  unique
+
+// object
+
+console.log(typeof "Rajesh");
+
+console.log(typeof null);
+console.log(typeof undefined);
